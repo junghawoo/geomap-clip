7dd26033f14228d88d4f6f970c614ba259405ddc
Jungha Woo <jungha.woo@gmail.com>
2025-03-30 18:29:24 -0400

    package ver 0.0.2 published. Added weights, csv to the package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="geomapclip",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     description="",
     long_description=open("README.md").read(),
@@ -16,6 +16,12 @@ setup(
     license="MIT",
     install_requires=requirements,
     include_package_data=True,
+    package_data={
+        "geomapclip.model": [
+            "weights/*.pth",
+            "gps_gallery/*.csv",
+        ]
+    },
     python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3",
